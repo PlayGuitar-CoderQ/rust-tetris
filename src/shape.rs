362098723fcf5ca96f83234e3a96bca9a8907b32
Declan Chiu <1282117217@qq.com>
2023-02-19 11:33:14 +0800

    feat: ✨ 新增shape 的方位所有权

--- src/shape.rs
+++ src/shape.rs
@@ -6,16 +6,29 @@ pub struct Pos(pub i32, pub i32);
 #[derive(Debug, Clone)]
 pub struct Shape {
     positions: HashSet<i32, i32>,
-    anchor: Pos
+    anchor: Pos,
+}
+
+macro_rules! impl_shape_constructor {
+    ($new:ident: [ $( $pos:expr ),* ] anchored at $anchor:expr) => {
+        pub fn new_i() -> Self {
+            Self {
+                positions: [Pos(0, 0), Pos(1, 0), Pos(2, 0), Pos(3, 0)]
+                    .into_iter()
+                    .collect(),
+                anchor: Pos(1, 0),
+            }
+        }
+    };
 }
 
 impl Shape {
     pub fn new_i() -> Self {
         Self {
-            positions: [Pos(0,0), Pos(1,0), Pos(2,0), Pos(3, 0)]
+            positions: [Pos(0, 0), Pos(1, 0), Pos(2, 0), Pos(3, 0)]
                 .into_iter()
                 .collect(),
-            anchor: Pos(1, 0)
+            anchor: Pos(1, 0),
         }
     }
 }
